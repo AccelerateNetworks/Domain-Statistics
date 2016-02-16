aaa152ce77e1951d8ed26b5620964261f611a66f
Finn Herzfeld <finn@callpipe.com>
2016-02-16 14:50:52 -0800

    Silly me, this isn't the billing app

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php if(if_group("superadmin")) { ?>
-  <br /><br /><br /><a href="githook.php">Check for billing app updates</a>
+  <br /><br /><br /><a href="githook.php">Check for app updates</a>
 <?php
 }
 require_once "resources/footer.php";
