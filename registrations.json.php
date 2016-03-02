490b593ddf21662d23862ba6716b0072a4503f72
dan <dan@seattlemesh.net>
2016-03-01 22:40:52 -0800

    whoopz!

--- registrations.json.php
+++ registrations.json.php
@@ -16,18 +16,21 @@ $args = array(
 );
 
 $fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
-if (!$fp) {
+if(!$fp) {
   die(json_encode(array("success" => failse, "reason" => "Failed to connect to freeswitch")));
 }
 
 $cmd = "api sofia xmlstatus profile internal reg";
 $xml_response = trim(event_socket_request($fp, $cmd));
 //TODO if xml_response is Invaild Profile! set off all the alarms!!!!
+
 $xml_response = str_replace("<profile-info>", "<profile_info>", $xml_response);
 $xml_response = str_replace("</profile-info>", "</profile_info>", $xml_response);
 $xml = new SimpleXMLElement($xml_response);
+
 $out = [];
-foreach ($xml->registrations->registration as $row) {				foreach ($xml->registrations->registration as $row) {
+
+foreach($xml->registrations->registration as $row) {
   $out[] = $row;
 }
 
