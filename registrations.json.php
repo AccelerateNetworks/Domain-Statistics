48658dca303dee199778653635f3966014fb395a
dan <dan@seattlemesh.net>
2016-03-01 23:20:49 -0800

    Limited results to API keys domain.

--- registrations.json.php
+++ registrations.json.php
@@ -10,10 +10,12 @@ require_once "utils.php";
 if(!isset($_REQUEST['key'])) {
   die(json_encode(array("success" => false, "reason" => "No API key specified")));
 }
+$domain_name = do_sql($db, "SELECT v_domains.domain_name FROM v_domains, cdr_api_keys WHERE cdr_api_keys.domain_uuid = v_domains.domain_uuid AND cdr_api_keys.key = :key",
+  array(':key' => $_REQUEST['key']));
 
-$args = array(
-  ':key' => $_REQUEST['key']
-);
+if(count(domain_name) != 1) {
+  die(json_encode(array("success" => false, "reason" => "Invalid API Key")));
+}
 
 $fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
 if(!$fp) {
@@ -31,7 +33,9 @@ $xml = new SimpleXMLElement($xml_response);
 $out = [];
 
 foreach($xml->registrations->registration as $row) {
-  $out[] = $row;
+  if($row['sip-auth-realm'] == $domain_name[0]['domain_name']){
+    $out[] = $row;
+  }
 }
 
 echo(json_encode($out));
