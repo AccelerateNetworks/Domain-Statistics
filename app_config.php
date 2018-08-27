060daa2001b88b7faa0aaec706378cb377c7a25f
Dan <dan@acceleratenetworks.com>
2018-08-27 15:07:32 -0700

    Removed unneeded databse table

--- app_config.php
+++ app_config.php
@@ -17,39 +17,3 @@ $apps[$x]['description']['fr-ca'] = "";
 $apps[$x]['description']['fr-ch'] = "";
 $apps[$x]['description']['pt-pt'] = "";
 $apps[$x]['description']['pt-br'] = "";
-
-$y = 0;
-$z = 0;
-$apps[$x]['db'][$y]['table'] = "cdr_api_keys";
-$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'token_uuid';
-$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-$apps[$x]['db'][$y]['fields'][$z]['description'] = '';
-$z++;
-
-$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'key';
-$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-$apps[$x]['db'][$y]['fields'][$z]['description'] = '';
-$z++;
-
-$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'name';
-$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-$apps[$x]['db'][$y]['fields'][$z]['description'] = '';
-$z++;
-
-$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'enabled';
-$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'boolean';
-$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'boolean';
-$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'tinyint(1)';
-$apps[$x]['db'][$y]['fields'][$z]['description'] = '';
-$z++;
-
-$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
-$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_domains';
-$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'domain_uuid';
-$apps[$x]['db'][$y]['fields'][$z]['description'] = '';
